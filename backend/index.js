4bd878e04c000c4116fd8a301ec9a63dfbdfab1c
alfaArghya <arghya.coding@gmail.com>
2024-02-11 11:32:37 +0530

    set validation

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,36 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send(`<h3>Server is working<h3/>`);
+});
+
+app.post("/todo", (req, res) => {
+  const createPayLoad = req.body;
+  const parsePayLoad = createTodo.safeParse(createPayLoad);
+  if (!parsePayLoad.success) {
+    res.status(411).json({
+      msg: "You sent the wrong input",
+    });
+    return;
+  }
+});
+
+app.get("/todo", (req, res) => {});
+
+app.put("/completed", (req, res) => {
+  const createPayLoad = req.body;
+  const parsePayLoad = updateTodo.safeParse(createPayLoad);
+  if (!parsePayLoad.success) {
+    res.status(411).json({
+      msg: "You sent the wrong input",
+    });
+    return;
+  }
 });
 
 app.listen(port, () => {
