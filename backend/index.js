63a17c56679e836a3c39ab15f1f86c66bbee0a4b
alfaArghya <arghya.coding@gmail.com>
2024-02-11 18:13:18 +0530

    add CORS

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,46 @@
 const express = require("express");
+const cors = require("cors");
 const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(`<h3>Server is working<h3/>`);
 });
 
-app.post("/todo", (req, res) => {
-  const createPayLoad = req.body;
-  const parsePayLoad = createTodo.safeParse(createPayLoad);
-  if (!parsePayLoad.success) {
+app.post("/todo", async (req, res) => {
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
+
+  if (!parsedPayload.success) {
     res.status(411).json({
-      msg: "You sent the wrong input",
+      msg: "You sent the wrong inputs",
     });
     return;
   }
+  // put it in mongodb
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false,
+  });
+
+  res.json({
+    msg: "Todo created",
+  });
 });
 
-app.get("/todo", (req, res) => {});
+app.get("/todo", async (req, res) => {
+  const todos = await todo.find({});
+  console.log(todos);
+  res.status(200).json({ todos });
+});
 
-app.put("/completed", (req, res) => {
+app.put("/completed", async (req, res) => {
   const createPayLoad = req.body;
   const parsePayLoad = updateTodo.safeParse(createPayLoad);
   if (!parsePayLoad.success) {
@@ -31,6 +49,19 @@ app.put("/completed", (req, res) => {
     });
     return;
   }
+
+  await todo.update(
+    {
+      _id: req.body.id,
+    },
+    {
+      completed: true,
+    }
+  );
+
+  res.json({
+    msg: "todo marked as completed",
+  });
 });
 
 app.listen(port, () => {
